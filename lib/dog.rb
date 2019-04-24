ce40138a0994daab6cf58734df738c1a04ad9826
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-24 18:56:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,7 @@ class Dog
 
      headers.collect.with_index do |header,i|
        binding.pry
-      #  result_hash[header] => result[i]
+        result_hash[header] = result[i]
      end
      binding.pry
 
