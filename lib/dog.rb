fdac945b0451ff7c66b3ce1f5f5d8ce264b4fa59
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-24 18:56:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,7 +49,7 @@ class Dog
      headers = DB[:conn].execute("PRAGMA table_info(dogs)").map {|col| col[1]}
 
      headers.collect.with_index do |header,i|
-       binding.pry
+      #  binding.pry
         result_hash[header] = result[i]
      end
      binding.pry
