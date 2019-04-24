1dd5042b74e5c2650f85990291e2367a6f8977dd
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-24 15:12:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,6 +8,13 @@ class Dog
   end
 
   def create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
   end
 
 end
